aecfad002ea91ed40332e2085c1e63516338a0ad
Jennifer Reuting <jen@reuting.com>
2022-11-07 11:33:56 -0800

    added the user register() method on the the User model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const bcrypt = require('bcrypt');
+const db = require("../db")
+
 /** User of the site. */
 
 class User {
@@ -9,6 +12,23 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await db.query(
+      `INSERT INTO users (
+        username,
+        password,
+        first_name,
+        last_name,
+        phone,
+        join_at)
+      VALUES
+        ($1, $2, $3, $4, $5, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]
+    )
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
