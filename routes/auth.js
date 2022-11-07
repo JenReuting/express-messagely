40db33feefe0cc3a892dfa38588a1079de60c980
Jennifer Reuting <jen@reuting.com>
2022-11-07 15:31:16 -0800

    sort of did the login route - still errors

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,34 @@
 const Router = require("express").Router;
 const router = new Router();
 
-/** POST /login: {username, password} => {token} */
+const { authenticateJWT } = require("../middleware/auth.js");
+const User = require("../models/user");
+const { UnauthorizedError, BadRequestError } = require("../expressError");
+const { SECRET_KEY } = require("../config");
 
+/** POST /login: {username, password} => {token} */
+/** Handles the login POST route. If successful: Takes in a username and password,
+ * updates the Login Timestamp, generates a new token and signs it, returns the
+ * token in the body of the response.
+ * If unsuccessful: Throws new BadRequestError if body is empty or Unauthorized
+ * Error if user/password invalid.
+ */
+router.post("/login", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const { username, password } = req.body;
+  const
+
+  //if verified, put token on res.locals.user
+  if (await User.authenticate(username, password)) {
+    const token = jwt.sign({username}, SECRET_KEY, {iat: 60*60 });
+    User.updateLoginTimestamp(username);
+    return res.json({_token: token});
+  } else {
+    throw new UnauthorizedError("Invalid username and/or password");
+  }
+
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
